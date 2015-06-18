6abaed9c4d3bdbd512b587dfde62b5b3fc7c474b
Benjamin Phillips <benred42@gmail.com>
2015-06-18 14:38:05 -0400

    Added bookmark and click models to links app

--- urly_bird/links/models.py
+++ urly_bird/links/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Bookmark(models.Model):
+    url = models.URLField(max_length=255)
+    code = models.CharField(max_length=255)
+    tile = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return str(self.title)
+
+#######################################################################################################################
+
+class Click(models.Model):
+    user = models.ForeignKey(User)
+    bookmark = models.ForeignKey(Bookmark)
+    timestamp = models.DateTimeField()
